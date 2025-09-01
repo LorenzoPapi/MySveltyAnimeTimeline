0b494eaca266ab502453571b1ae12b526f964dfc
LorenzoPapi <topolino123654@gmail.com>
2025-09-01 20:40:26 +0200

    giving up on gh pages deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,5 @@
-import adapter from '@sveltejs/adapter-static'
+import adapter from '@sveltejs/adapter-auto'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-
-const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -13,16 +11,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter({
-			pages: "build",
-			assets: 'build',
-			fallback: "app.html",
-			precompress: false,
-			strict: true
-		}),
-		paths: {
-			base: dev ? '' : process.env.BASE_PATH
-		}
+		adapter: adapter()
 	}
 };
 
